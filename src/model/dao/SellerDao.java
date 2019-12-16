49c473a78f723bcd1c39e5b689ad076c58011907
ronaldo-melo <rpm.melkor@gmail.com>
2019-12-16 17:19:57 -0300

    findById implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,5 +1,17 @@
 package model.dao;
 
-public interface SellerDao {
+import java.util.List;
 
+import model.entities.Seller;
+
+public interface SellerDao {
+	void insert(Seller obj);
+	
+	void update(Seller obj);
+	
+	void delete(Integer id);
+	
+	Seller findById(Integer id);
+	
+	List<Seller> findAll();	
 }
