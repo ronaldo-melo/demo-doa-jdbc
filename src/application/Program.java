582a44ccfe357523ea81229f414fe0891e986b3f
ronaldo-melo <rpm.melkor@gmail.com>
2019-12-17 12:10:36 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -9,7 +11,7 @@ import model.entities.Seller;
 
 public class Program {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException{
 			
 		SellerDao sellerDao = DaoFactory.createSellerDao(); 
 		
@@ -20,8 +22,18 @@ public class Program {
 		System.out.println("\n=== TEST 2: seller findByDepartment =====\n");
 		Department department = new Department(1, null);
 		
+		System.out.println("\n=== TEST 3: seller insert =====\n");
 		List<Seller> list = sellerDao.findByDepartment(department);
 		list.stream().forEach(System.out::println);
+		
+		System.out.println("\n=== TEST 4: seller insert =====\n");
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", sdf.parse("22/06/1994"), 4000.0, department);
+		sellerDao.insert(newSeller);
+		
+		System.out.println("Inserted! New id = " + newSeller.getId());
 		
 	}
 	
